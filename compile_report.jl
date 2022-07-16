0ecaaaa0fe94d30c8fb824ac5a73838421a38ca6
Vivek Srikrishnan <vs498@cornell.edu>
2022-07-15 20:10:05 -0400

    add option for HTML compile

--- compile_report.jl
+++ compile_report.jl
@@ -6,12 +6,14 @@ using Weave
 
 function compile_report(fname, out_type = "pdf")
     if out_type == "pdf"
-        Weave.weave(fname, doctype="md2pdf", template="bee4750.tpl", fig_path="figures")
+        Weave.weave(fname, doctype="pandoc2pdf", template="bee4750.tpl", fig_path="figures")
     elseif out_type == "html"
         Weave.weave(fname, doctype="md2html", fig_path="figures")
+    elseif out_type == "html-pandoc"
+        Weave.weave(fname, doctype="pandoc2html", template="bee4750.tpl", fig_path="figures")
     else
-        throw(ArgumentError("out_type must be pdf or html"))
-    end
+        throw(ArgumentError("out_type must be pdf, html, or html-pandoc"))
+    end 
 end
 
 if !isinteractive()
